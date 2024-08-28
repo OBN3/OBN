ce867d9ddf9111b371041e7287bfafe915dd547e
OBN3 <ofirbn@gmail.com>
2024-08-28 11:04:55 +0300

    Update save_results.php

--- save_results.php
+++ save_results.php
@@ -1,16 +1,12 @@
 <?php
-header('Content-Type: application/json');
-
-$data = json_decode(file_get_contents('php://input'), true);
-
-if (isset($data['csvContent'])) {
+if (isset($_POST['csvContent'])) {
     $filename = 'results.csv';
     
     // Append to file if it exists, create if it doesn't
-    file_put_contents($filename, $data['csvContent'], FILE_APPEND | LOCK_EX);
+    file_put_contents($filename, $_POST['csvContent'], FILE_APPEND | LOCK_EX);
     
-    echo json_encode(['status' => 'success', 'message' => 'Results saved successfully']);
+    echo 'Results saved successfully';
 } else {
     http_response_code(400);
-    echo json_encode(['status' => 'error', 'message' => 'Invalid data received']);
+    echo 'Invalid data received';
 }
